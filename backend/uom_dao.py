1c3e4ee531055013764140899ec35ddfaa2dd281
Sneha Bansal <116899115+snehabansal483@users.noreply.github.com>
2025-04-02 12:15:17 +0530

    Update uom_dao.py

--- backend/uom_dao.py
+++ backend/uom_dao.py
@@ -1,3 +1,4 @@
+from backend.sql_connection import get_sql_connection
 
 def get_uoms(connection):
     cursor = connection.cursor()
@@ -13,8 +14,6 @@ def get_uoms(connection):
 
 
 if __name__ == '__main__':
-    from sql_connection import get_sql_connection
-
     connection = get_sql_connection()
     # print(get_all_products(connection))
     print(get_uoms(connection))
